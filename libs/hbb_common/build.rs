6586e7755d906ad51a9c6fdb0cd0d418b071cbfd
purezhi <purezhi@gmail.com>
2021-11-06 15:09:24 +0800

    Common update

--- libs/hbb_common/build.rs
+++ libs/hbb_common/build.rs
@@ -1,6 +1,9 @@
+use protobuf_codegen::Codegen;
+
 fn main() {
     std::fs::create_dir_all("src/protos").unwrap();
-    protobuf_codegen_pure::Codegen::new()
+    Codegen::new()
+        .pure()
         .out_dir("src/protos")
         .inputs(&["protos/rendezvous.proto", "protos/message.proto"])
         .include("protos")
